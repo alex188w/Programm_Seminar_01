e42e642916e0d98ee976d0a0073a87c44fee7d5b
Алексей Шевченко <alex18w@mail.ru>
2022-07-10 13:08:56 +0500

    Добавлена информация по оформлению задачи в процедуру

---
+++ Sadacha_7/Program.cs
@@ -0,0 +1,14 @@
+﻿void Sadacha_7() //оформлена в виде процедуры
+{
+    int number = 7;
+    int start = -number;
+    while (start <= number)
+    {
+        Console.Write(start + " ");
+        start++; //увеличиваем start на единицу
+    }
+
+    Console.WriteLine();
+}
+
+Sadacha_7();
