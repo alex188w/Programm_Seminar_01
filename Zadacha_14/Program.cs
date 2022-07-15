1639458e7ac5fc56f09768655578ad805c208e3a
Алексей Шевченко <alex18w@mail.ru>
2022-07-15 22:01:04 +0500

    Задача 14 - сложное условие &&

---
+++ Zadacha_14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int Number1 =  Convert.ToInt32(Console.ReadLine());
+int a =  7;
+int b = 23;
+int div1 = Number1 % a;
+int div2 = Number1 % b;
+
+if (div1 == 0 && div2 == 0) //сложное условие - объединение двух условий
+Console.WriteLine("Введенное число является кратным 7 и 23");
+else 
+Console.WriteLine("Введенное число не является кратным 7 и 23");
