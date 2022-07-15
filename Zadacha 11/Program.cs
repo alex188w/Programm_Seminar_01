9a4fad4354161e7c7d3875b28df01c891eafb03d
Алексей Шевченко <alex18w@mail.ru>
2022-07-15 21:11:59 +0500

    Задача 11

---
+++ Zadacha 11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого 
+//числа
+
+Random rand = new Random();
+int number = rand.Next(100, 1000); // [10, 100)
+Console.WriteLine("Случайное число");
+Console.WriteLine(number);
+
+int ones = number % 10;
+int tens = number / 10 % 10; // пример разбивки трехзначного числа на составляющие цифры
+int hungs = number / 100;
+
+int result = ones + hungs * 10;
+Console.WriteLine(result);
